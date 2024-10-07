9a43c87029def305861ff96ca28ba4b1706a94e5
kigarde <kigardetom2001@gmail.com>
2024-10-07 10:40:02 +0300

    Update packages and add env

--- src/lib/state.svelte.ts
+++ src/lib/state.svelte.ts
@@ -1,16 +1,10 @@
-import { getContext, setContext } from 'svelte';
-
 class State {
 	//create type of user
+	
 
 	user: { id: string; name: string } = $state({ id: '', name: '' });
-	STATE_ID: string = 'state_id';
+	STATE_ID: symbol = Symbol('state_id');
 	constructor() {}
 }
-const state = new State();
-export const setStateContext = () => {
-	setContext(state.STATE_ID, state);
-};
-export const getStateContext = (state_id: string) => {
-	return getContext(state_id);
-};
+const state = $state(new State());
+export default state;
