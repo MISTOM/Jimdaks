c54c63b9fdd3237c552f4aad39119629833f43d0
kigarde <kigardetom2001@gmail.com>
2024-11-19 12:57:50 +0300

    add toast component and features, add register page

--- src/routes/(Auth)/register/+page.server.ts
+++ src/routes/(Auth)/register/+page.server.ts
@@ -1,5 +1,84 @@
+import { fail, redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import type { Actions } from './$types';
+import auth from '$lib/server/auth';
+import prisma from '$lib/server/prisma';
+import { maxAge, refreshTokenMaxAge } from '$lib/server/utils';
+import { NODE_ENV } from '$env/static/private';
 
-export const load = (async () => {
-	return {};
+export const load = (async ({ locals }) => {
+	// if (locals.user) { throw redirect(303, '/') }
 }) satisfies PageServerLoad;
+
+export const actions = {
+	default: async ({ request, cookies }) => {
+		const formData = await request.formData();
+		console.log(formData);
+		const name = formData.get('name');
+		const email = formData.get('email');
+		const password = formData.get('password');
+		const confirmPassword = formData.get('confirmPassword');
+
+		if (!name || !email || !password) {
+			return fail(400, {
+				data: { name, email },
+				errors: 'Please fill in all fields'
+			});
+		}
+
+		if (password !== confirmPassword) {
+			return fail(400, {
+				data: { name, email },
+				errors: 'Passwords do not matchhh'
+			});
+		}
+
+		try {
+			const user = await prisma.user.findUnique({
+				where: {
+					email: email.toString()
+				}
+			});
+			if (user) {
+				return fail(400, {
+					data: { name, email },
+					errors: 'User already exists'
+				});
+			}
+			const hashedPassword = await auth.hash(password.toString());
+			const newUser = await prisma.user.create({
+				data: {
+					name: name.toString(),
+					email: email.toString(),
+					password: hashedPassword,
+					role: {
+						connect: {
+							name: 'CARETAKER'
+						}
+					}
+				}
+			});
+
+			// Register user and Login immediately
+			const token = auth.sign(newUser);
+			const refreshToken = await auth.generateRefreshToken(newUser);
+
+			cookies.set('token', token, { httpOnly: true, secure: NODE_ENV === 'production', path: '/', maxAge });
+			cookies.set('refreshToken', refreshToken, {
+				httpOnly: true,
+				secure: NODE_ENV === 'production',
+				path: '/',
+				maxAge: refreshTokenMaxAge
+			});
+
+			console.log('User created', formData, hashedPassword);
+		} catch (e) {
+			console.log(e);
+			return fail(500, {
+				data: { name, email, password },
+				errors: 'An error occurred'
+			});
+		}
+		throw redirect(303, '/product');
+	}
+} satisfies Actions;
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,13 @@
 <script>
+	import Toaster from '$lib/components/Toaster.svelte';
+	import { setToastState } from '$lib/Toast.svelte';
 	import '../app.css';
 	let { children } = $props();
+
+	setToastState();
 </script>
 
 <noscript>Please Enable Javascript for this app to run</noscript>
+<Toaster />
 {@render children?.()}
 <footer></footer>
