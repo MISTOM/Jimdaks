6b0a8002625893d34758cdf949b88142cde851f8
kigarde <kigardetom2001@gmail.com>
2024-12-03 14:18:26 +0300

    protect dashboard route to authorised users, implement feed inventory management.

--- src/routes/dashboard/inventory/+page.svelte
+++ src/routes/dashboard/inventory/+page.svelte
@@ -1,64 +1,105 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import { slide } from 'svelte/transition';
 	import type { PageData } from './$types';
+	import { enhance } from '$app/forms';
 
 	let { data }: { data: PageData } = $props();
 
 	let feedTypeFilter = $state('');
 	let sortBy = $state('createdAt');
 	let sortOrder = $state('desc');
+	let openFormFeedType = $state('');
+	let formErrors = $state();
 
 	let inventoryData = $derived(data.feedInventoryMap);
+	let feedInventory = $derived(data.feedInventory);
 
 	const feedTypes = Object.values(data.FeedType);
+	// $inspect('Feed inventory: ', inventoryData
 
-	const applyFilters = () => {
-		const params = new URLSearchParams();
-		if (feedTypeFilter) params.set('feedType', feedTypeFilter);
-		if (sortBy) params.set('sortBy', sortBy);
-		if (sortOrder) params.set('sortOrder', sortOrder);
-		goto(`/dashboard/inventory/?${params.toString()}`);
+	const toggleForm = (feedType: string) => {
+		openFormFeedType = openFormFeedType === feedType ? '' : feedType;
+		formErrors = '';
 	};
-
-	$inspect('Feed inventory: ', inventoryData);
 </script>
 
 <h1 class="mb-4 text-2xl font-bold">Feed Inventory</h1>
 
 <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
-	{#each feedTypes as feedType}
+	{#each feedTypes as feedType, i (feedType)}
 		<div class="flex flex-col justify-between rounded-lg border p-4 shadow-sm hover:shadow-md">
 			<div>
 				<h2 class="mb-2 text-xl font-semibold">{feedType.replace('_', ' ')}</h2>
 				<p>Notes: {inventoryData[feedType]?.notes}</p>
 				<p class="mb-2 text-4xl font-bold">
-					{inventoryData[feedType]?._sum.quantity ?? 0}
+					{inventoryData[feedType]?.quantity ?? 0}
 					<span class="text-lg font-medium"> kg</span>
 				</p>
-				<!-- Progress bar (Example using a max capacity of 1000 kg) -->
+				<!-- Progress bar ( 500 kg max) -->
+			</div>
+			<div class="flex items-center justify-center">
 				<div class="mb-4 h-2 w-full rounded-full bg-gray-200">
 					<div
-						class="h-2 rounded-full bg-green-500"
-						style="width: {Math.min(
-							100,
-							((inventoryData[feedType]?._sum.quantity ?? 0) / 1000) * 100
-						)};"
+						class="h-2 rounded-full bg-green-700"
+						style="width: {Math.min(((inventoryData[feedType]?.quantity ?? 0) / 500) * 100, 100)}%"
 					></div>
 				</div>
+				<span class="ml-2 text-center text-sm text-gray-600">500KG</span>
 			</div>
-			<div class="flex space-x-2">
+			<div class="flex space-x-5">
 				<button
-					class="rounded bg-green-600 px-3 py-2 text-white transition-colors hover:bg-green-700"
-					onclick={() => console.log('Add stock clicked!')}
+					class="rounded bg-green-700 px-3 py-2 text-white transition-colors hover:bg-green-800"
+					onclick={() => toggleForm(feedType)}
 				>
-					Add Stock
+					{openFormFeedType === feedType ? 'Cancel' : 'Add Stock'}
 				</button>
-				<button
-					class="self-center text-green-600 transition-colors hover:underline"
-					onclick={() => console.log('View details clicked!')}
-				>
-					View Details
-				</button>
+				{#if openFormFeedType === feedType}
+					<form
+						method="POST"
+						class="mt-2"
+						in:slide
+						out:slide={{ duration: 150 }}
+						use:enhance={() => {
+							return async ({ result, update }) => {
+								if (result.type === 'success') {
+									await update();
+									toggleForm(feedType);
+								}
+								if (result.type === 'failure') {
+									formErrors = result?.data?.error ? result.data.error : 'Error adding feed stock';
+								}
+							};
+						}}
+					>
+						{#if formErrors}
+							<span class="text-sm text-red-600">{formErrors}</span>
+						{/if}
+						<div class="flex items-center space-x-5">
+							<input
+								type="number"
+								name="quantity"
+								min="0"
+								step="10"
+								placeholder="Quantity (kg)"
+								class="w-full rounded border px-3 py-2"
+							/>
+							<input
+								type="text"
+								name="notes"
+								placeholder="Notes"
+								class="w-full rounded border px-3 py-2"
+							/>
+							<input type="hidden" name="feedType" value={feedTypes[i]} />
+							<button
+								type="submit"
+								class="rounded bg-green-700 px-3 py-2 text-white transition-colors hover:bg-green-800"
+							>
+								Submit
+							</button>
+						</div>
+					</form>
+				{/if}
 			</div>
 		</div>
 	{/each}
