989a0322e006e358328833108814f1ef10992f64
kigarde <kigardetom2001@gmail.com>
2024-12-06 08:51:12 +0300

    return flock page

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -46,7 +46,7 @@
 
 	<div class="flex-1 p-6">
 		<div class="absolute right-4 top-4">
-			<button class="rounded border px-4 py-2 text-sm font-semibold" on:click={toggleTheme}>
+			<button class="rounded border px-4 py-2 text-sm font-semibold" onclick={toggleTheme}>
 				{#if $isDarkMode}
 					🌙 Dark Mode
 				{:else}
