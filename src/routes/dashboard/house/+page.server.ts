890d98544e1cd5004746d8ae046517192c68b03d
kigarde <kigardetom2001@gmail.com>
2024-12-11 17:03:58 +0300

    Implement house deletion API and enhance house management with form handling and search functionality

--- src/routes/dashboard/house/+page.server.ts
+++ src/routes/dashboard/house/+page.server.ts
@@ -1,7 +1,9 @@
 import prisma from '$lib/server/prisma';
-import type { PageServerLoad } from './$types';
+import { fail } from '@sveltejs/kit';
+import type { Actions, PageServerLoad } from './$types';
 
-export const load = (async () => {
+export const load = (async ({ depends }) => {
+	depends('update:houses');
 	try {
 		const houses = await prisma.house.findMany();
 
@@ -10,3 +12,26 @@ export const load = (async () => {
 		console.error(e);
 	}
 }) satisfies PageServerLoad;
+
+export const actions: Actions = {
+	house: async ({ request }) => {
+		const data = Object.fromEntries((await request.formData()).entries());
+		const { name, capacity, description } = data;
+
+		if (!name || !capacity) return fail(400, { error: 'Missing fields' });
+
+		try {
+			const house = await prisma.house.create({
+				data: {
+					name: name.toString(),
+					capacity: parseInt(capacity.toString()),
+					description: description ? description.toString() : undefined
+				}
+			});
+
+			return { status: 201 };
+		} catch (e) {
+			console.error(e);
+		}
+	}
+};
